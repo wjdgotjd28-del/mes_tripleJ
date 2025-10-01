d6352f340eb2280dc7d411542e3b5908fbfd9b92
JYB0535 <jnk8528@gmail.com>
2025-10-01 14:26:51 +0900

    mes_tripleJ_back
    최초 커밋

---
+++ mes_back/src/main/java/com/mes_back/MesBackApplication.java
@@ -0,0 +1,13 @@
+package com.mes_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesBackApplication.class, args);
+	}
+
+}
---
+++ mes_back/src/test/java/com/mes_back/MesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mes_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
