0da4aa43d6217ac22ee531987f0c775f0f6b0cc7
JYB0535 <jnk8528@gmail.com>
2025-10-14 15:21:57 +0900

    나갈때 취소

--- mes_front/src/masterData/companies/pages/BusinessPartnerDetailModal.tsx
+++ mes_front/src/masterData/companies/pages/BusinessPartnerDetailModal.tsx
@@ -1,5 +1,16 @@
 import * as React from "react";
-import { Box, Button, TextField, MenuItem, Typography, Modal } from "@mui/material";
+import {
+  Box,
+  Button,
+  TextField,
+  MenuItem,
+  Typography,
+  Modal,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 
 type Company = {
   id: number;
@@ -48,6 +59,7 @@ export default function BusinessPartnerDetailModal({
   const [isEditing, setIsEditing] = React.useState(false);
   const [formData, setFormData] = React.useState<Company | null>(company);
   const [backupData, setBackupData] = React.useState<Company | null>(null);
+  const [confirmOpen, setConfirmOpen] = React.useState(false); // ✅ 확인 다이얼로그 상태
 
   React.useEffect(() => {
     setFormData(company);
@@ -69,15 +81,31 @@ export default function BusinessPartnerDetailModal({
   const handleSave = () => {
     if (formData) {
       onSave(formData);
-      setBackupData(formData); // 저장 시 최신 데이터 백업
+      setBackupData(formData);
       setIsEditing(false);
     }
   };
 
   const handleCancel = () => {
-    // 수정 취소 시 원래 데이터로 복구
+    // 수정 중이면 경고창 띄우기
+    if (isEditing) {
+      setConfirmOpen(true);
+    } else {
+      setIsEditing(false);
+      onClose();
+    }
+  };
+
+  const confirmCancel = () => {
+    // 예 눌렀을 때
     if (backupData) setFormData(backupData);
     setIsEditing(false);
+    setConfirmOpen(false);
+  };
+
+  const cancelDialogClose = () => {
+    // 아니오 눌렀을 때
+    setConfirmOpen(false);
   };
 
   const handleCloseModal = () => {
@@ -86,208 +114,222 @@ export default function BusinessPartnerDetailModal({
   };
 
   return (
-    <Modal open={open} onClose={handleCloseModal}>
-      <Box sx={style}>
-        <Typography
-          variant="h6"
-          sx={{ mb: 2, fontWeight: 600, textAlign: "center" }}
-        >
-          업체 상세 조회
-        </Typography>
+    <>
+      <Modal open={open} onClose={handleCloseModal}>
+        <Box sx={style}>
+          <Typography
+            variant="h6"
+            sx={{ mb: 2, fontWeight: 600, textAlign: "center" }}
+          >
+            업체 상세 조회
+          </Typography>
 
-        <Box
-          sx={{ flexGrow: 1, mb: 3, display: "flex", flexDirection: "column" }}
-        >
-          {/* 업체 유형 */}
-          {isEditing ? (
+          <Box
+            sx={{ flexGrow: 1, mb: 3, display: "flex", flexDirection: "column" }}
+          >
+            {isEditing ? (
+              <TextField
+                select
+                fullWidth
+                size="small"
+                label="업체 유형"
+                name="type"
+                value={formData.type}
+                onChange={handleChange}
+                sx={{ mb: 2 }}
+              >
+                <MenuItem value="거래처">거래처</MenuItem>
+                <MenuItem value="매입처">매입처</MenuItem>
+              </TextField>
+            ) : (
+              <TextField
+                fullWidth
+                size="small"
+                label="업체 유형"
+                value={formData.type}
+                InputProps={{ readOnly: true }}
+                sx={{ mb: 2 }}
+              />
+            )}
+
             <TextField
-              select
               fullWidth
               size="small"
-              label="업체 유형"
-              name="type"
-              value={formData.type}
+              label="사업자등록번호"
+              name="businessNumber"
+              value={formData.businessNumber}
               onChange={handleChange}
-              sx={{ mb: 2 }}
-            >
-              <MenuItem value="거래처">거래처</MenuItem>
-              <MenuItem value="매입처">매입처</MenuItem>
-            </TextField>
-          ) : (
-            <TextField
-              fullWidth
-              size="small"
-              label="업체 유형"
-              value={formData.type}
-              InputProps={{ readOnly: true }}
+              InputProps={{ readOnly: !isEditing }}
               sx={{ mb: 2 }}
             />
-          )}
-
-          <TextField
-            fullWidth
-            size="small"
-            label="사업자등록번호"
-            name="businessNumber"
-            value={formData.businessNumber}
-            onChange={handleChange}
-            InputProps={{ readOnly: !isEditing }}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            fullWidth
-            size="small"
-            label="업체명"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            InputProps={{ readOnly: !isEditing }}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            fullWidth
-            size="small"
-            label="대표명"
-            name="ceo"
-            value={formData.ceo}
-            onChange={handleChange}
-            InputProps={{ readOnly: !isEditing }}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            fullWidth
-            size="small"
-            label="대표전화번호"
-            name="ceoPhone"
-            value={formData.ceoPhone}
-            onChange={handleChange}
-            InputProps={{ readOnly: !isEditing }}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            fullWidth
-            size="small"
-            label="담당자명"
-            name="managerName"
-            value={formData.managerName}
-            onChange={handleChange}
-            InputProps={{ readOnly: !isEditing }}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            fullWidth
-            size="small"
-            label="담당자전화번호"
-            name="managerPhone"
-            value={formData.managerPhone}
-            onChange={handleChange}
-            InputProps={{ readOnly: !isEditing }}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            fullWidth
-            size="small"
-            label="담당자 이메일"
-            name="managerEmail"
-            value={formData.managerEmail}
-            onChange={handleChange}
-            InputProps={{ readOnly: !isEditing }}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            fullWidth
-            size="small"
-            label="주소"
-            name="address"
-            value={formData.address}
-            onChange={handleChange}
-            InputProps={{ readOnly: !isEditing }}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            fullWidth
-            size="small"
-            label="비고"
-            name="note"
-            value={formData.note}
-            onChange={handleChange}
-            multiline
-            rows={2}
-            InputProps={{ readOnly: !isEditing }}
-            sx={{ mb: 2 }}
-          />
-
-          {/* 거래 상태 */}
-          {isEditing ? (
             <TextField
-              select
               fullWidth
               size="small"
-              label="거래 상태"
-              name="status"
-              value={formData.status}
+              label="업체명"
+              name="name"
+              value={formData.name}
               onChange={handleChange}
-              sx={{ mb: 2 }}
-            >
-              <MenuItem value="거래중">거래중</MenuItem>
-              <MenuItem value="거래 종료">거래 종료</MenuItem>
-            </TextField>
-          ) : (
-            <TextField
-              fullWidth
-              size="small"
-              label="거래 상태"
-              name="status"
-              value={formData.status}
-              InputProps={{ readOnly: true }}
+              InputProps={{ readOnly: !isEditing }}
               sx={{ mb: 2 }}
             />
-          )}
-        </Box>
+            <TextField
+              fullWidth
+              size="small"
+              label="대표명"
+              name="ceo"
+              value={formData.ceo}
+              onChange={handleChange}
+              InputProps={{ readOnly: !isEditing }}
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              fullWidth
+              size="small"
+              label="대표전화번호"
+              name="ceoPhone"
+              value={formData.ceoPhone}
+              onChange={handleChange}
+              InputProps={{ readOnly: !isEditing }}
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              fullWidth
+              size="small"
+              label="담당자명"
+              name="managerName"
+              value={formData.managerName}
+              onChange={handleChange}
+              InputProps={{ readOnly: !isEditing }}
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              fullWidth
+              size="small"
+              label="담당자전화번호"
+              name="managerPhone"
+              value={formData.managerPhone}
+              onChange={handleChange}
+              InputProps={{ readOnly: !isEditing }}
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              fullWidth
+              size="small"
+              label="담당자 이메일"
+              name="managerEmail"
+              value={formData.managerEmail}
+              onChange={handleChange}
+              InputProps={{ readOnly: !isEditing }}
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              fullWidth
+              size="small"
+              label="주소"
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+              InputProps={{ readOnly: !isEditing }}
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              fullWidth
+              size="small"
+              label="비고"
+              name="note"
+              value={formData.note}
+              onChange={handleChange}
+              multiline
+              rows={2}
+              InputProps={{ readOnly: !isEditing }}
+              sx={{ mb: 2 }}
+            />
 
-        {/* 하단 버튼 영역 */}
-        <Box sx={{ display: "flex", justifyContent: "flex-end", mt: "auto" }}>
-          {!isEditing ? (
-            <>
-              <Button
-                variant="outlined"
+            {isEditing ? (
+              <TextField
+                select
+                fullWidth
                 size="small"
-                onClick={() => setIsEditing(true)}
-                sx={{ mr: 1 }}
+                label="거래 상태"
+                name="status"
+                value={formData.status}
+                onChange={handleChange}
+                sx={{ mb: 2 }}
               >
-                수정
-              </Button>
-              <Button
-                variant="contained"
+                <MenuItem value="거래중">거래중</MenuItem>
+                <MenuItem value="거래 종료">거래 종료</MenuItem>
+              </TextField>
+            ) : (
+              <TextField
+                fullWidth
                 size="small"
-                onClick={handleCloseModal}
-              >
-                닫기
-              </Button>
-            </>
-          ) : (
-            <>
-              <Button
-                variant="contained"
-                size="small"
-                color="primary"
-                onClick={handleSave}
-                sx={{ mr: 1 }}
-              >
-                저장
-              </Button>
-              <Button
-                variant="outlined"
-                size="small"
-                color="inherit"
-                onClick={handleCancel}
-              >
-                취소
-              </Button>
-            </>
-          )}
+                label="거래 상태"
+                name="status"
+                value={formData.status}
+                InputProps={{ readOnly: true }}
+                sx={{ mb: 2 }}
+              />
+            )}
+          </Box>
+
+          {/* 하단 버튼 */}
+          <Box sx={{ display: "flex", justifyContent: "flex-end", mt: "auto" }}>
+            {!isEditing ? (
+              <>
+                <Button
+                  variant="outlined"
+                  size="small"
+                  onClick={() => setIsEditing(true)}
+                  sx={{ mr: 1 }}
+                >
+                  수정
+                </Button>
+                <Button
+                  variant="contained"
+                  size="small"
+                  onClick={handleCloseModal}
+                >
+                  닫기
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button
+                  variant="contained"
+                  size="small"
+                  color="primary"
+                  onClick={handleSave}
+                  sx={{ mr: 1 }}
+                >
+                  저장
+                </Button>
+                <Button
+                  variant="outlined"
+                  size="small"
+                  color="inherit"
+                  onClick={handleCancel}
+                >
+                  취소
+                </Button>
+              </>
+            )}
+          </Box>
         </Box>
-      </Box>
-    </Modal>
+      </Modal>
+
+      {/* ✅ 취소 확인 다이얼로그 */}
+      <Dialog open={confirmOpen} onClose={cancelDialogClose}>
+        <DialogTitle>저장하지 않고 나가시겠습니까?</DialogTitle>
+        <DialogContent>
+          변경된 내용은 저장되지 않습니다.
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={cancelDialogClose}>아니오</Button>
+          <Button color="error" onClick={confirmCancel}>
+            예
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
