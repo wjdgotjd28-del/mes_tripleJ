9fe86dd5c15485735718f7728829aa911086285b
JYB0535 <jnk8528@gmail.com>
2025-10-15 18:53:17 +0900

    주석 단거

--- mes_back/src/main/java/com/mes_back/controller/CompanyController.java
+++ mes_back/src/main/java/com/mes_back/controller/CompanyController.java
@@ -17,22 +17,25 @@ import java.util.List;
 public class CompanyController {
     private final CompanyService companyService;
 
-
+    // 업체 등록
     @PostMapping("/new")
     public CompanyDto addCompany(@RequestBody CompanyDto companyDto) {
         return companyService.addCompany(companyDto);
     }
 
+    // 업체 조회
     @GetMapping("")
     public List<CompanyDto> getCompanies() {
         return companyService.findAll();
     }
 
+    // 업체 수정
     @PutMapping("")
     public CompanyDto updateCompany(@RequestBody CompanyDto companyDto) {
         return companyService.updateCompany(companyDto);
     }
 
+    // 거래 상태 버튼 클릭 -> 거래 상태 변경
     @PatchMapping("/{companyId}/updateTradeStatus")
     public ResponseEntity<CompanyDto> updateTradeStatus(@PathVariable Long companyId, @RequestBody CompanyDto companyDto) {
          CompanyDto newStatus = companyService.updateTradeStatus(companyId, companyDto.getStatus());
