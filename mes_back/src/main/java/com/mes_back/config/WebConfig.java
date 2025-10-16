f19627ec8e46d6e42288525f45ce86f994bc29a0
JYB0535 <jnk8528@gmail.com>
2025-10-16 14:33:52 +0900

    Patch 안되는거 수정

--- mes_back/src/main/java/com/mes_back/config/WebConfig.java
+++ mes_back/src/main/java/com/mes_back/config/WebConfig.java
@@ -9,8 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOriginPatterns("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
