b74b935447ddfe56f75508bbd2259ee7b021230a
wjdgotjd28-del <wjdgotjd28@gmail.com>
2025-10-20 15:17:10 +0900

    change dto name

--- mes_back/src/main/java/com/mes_back/repository/OrderInboundRepositoryCustom.java
+++ mes_back/src/main/java/com/mes_back/repository/OrderInboundRepositoryCustom.java
@@ -1,9 +1,10 @@
 package com.mes_back.repository;
 
-import com.mes_back.dto.OrderInboundDto;
+
+import com.mes_back.dto.OrderInboundDTO;
 
 import java.util.List;
 
 public interface OrderInboundRepositoryCustom {
-    List<OrderInboundDto> findInboundHistoriesForOutbound();
+    List<OrderInboundDTO> findInboundHistoriesForOutbound();
 }
