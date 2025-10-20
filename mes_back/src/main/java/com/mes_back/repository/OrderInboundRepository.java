8f7ea789176fdef4d70e673d9b2b80bcca6efcda
JYB0535 <jnk8528@gmail.com>
2025-10-20 11:55:51 +0900

    중간저장용
    ㅇ

--- mes_back/src/main/java/com/mes_back/repository/OrderInboundRepository.java
+++ mes_back/src/main/java/com/mes_back/repository/OrderInboundRepository.java
@@ -1,28 +1,8 @@
 package com.mes_back.repository;
 
-import com.mes_back.dto.OrderInboundDto;
 import com.mes_back.entity.OrderInbound;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import java.util.List;
 
-public interface OrderInboundRepository extends JpaRepository<OrderInbound, Long> {
-    @Query("""
-    SELECT new com.mes_back.dto.OrderInboundDto(
-        oi.orderInboundId,
-        oi.lotNo,
-        oi.company.companyName,
-        oi.itemName,
-        oi.itemCode,
-        oi.inboundDate,
-        oi.qty - COALESCE(
-            (SELECT SUM(oo.qty) FROM OrderOutbound oo WHERE oo.orderInbound = oi), 
-            0
-        ),
-        oi.category
-    )
-    FROM OrderInbound oi
-""")
-    List<OrderInboundDto> findInboundHistoriesForOutbound();
+public interface OrderInboundRepository extends JpaRepository<OrderInbound, Long>, OrderInboundRepositoryCustom {
 
 }
