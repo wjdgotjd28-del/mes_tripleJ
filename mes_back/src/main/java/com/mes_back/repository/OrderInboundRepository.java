b125a64934e562aefdff74759eba1db9d277f840
JYB0535 <jnk8528@gmail.com>
2025-10-20 11:27:41 +0900

    11
    1

--- mes_back/src/main/java/com/mes_back/repository/OrderInboundRepository.java
+++ mes_back/src/main/java/com/mes_back/repository/OrderInboundRepository.java
@@ -7,16 +7,22 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface OrderInboundRepository extends JpaRepository<OrderInbound, Long> {
-
-    @Query("SELECT new com.mes_back.dto.OrderInboundDto(" +
-           "oi.orderInboundId, " +
-           "oi.lotNo, " +
-           "oi.company.companyName, " +
-           "oi.itemName, " +
-           "oi.itemCode, " +
-           "oi.inboundDate, " +
-           "(oi.qty - (SELECT COALESCE(SUM(oo.qty), 0L) FROM OrderOutbound oo WHERE oo.orderInbound = oi)), " +
-           "oi.category) " +
-           "FROM OrderInbound oi")
+    @Query("""
+    SELECT new com.mes_back.dto.OrderInboundDto(
+        oi.orderInboundId,
+        oi.lotNo,
+        oi.company.companyName,
+        oi.itemName,
+        oi.itemCode,
+        oi.inboundDate,
+        oi.qty - COALESCE(
+            (SELECT SUM(oo.qty) FROM OrderOutbound oo WHERE oo.orderInbound = oi), 
+            0
+        ),
+        oi.category
+    )
+    FROM OrderInbound oi
+""")
     List<OrderInboundDto> findInboundHistoriesForOutbound();
+
 }
