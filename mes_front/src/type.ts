dbf3a84211b9411a7dee983135c750eff8d2a226
JYB0535 <jnk8528@gmail.com>
2025-10-20 19:04:11 +0900

    ㅇ
    ㅇ

--- mes_front/src/type.ts
+++ mes_front/src/type.ts
@@ -141,3 +141,19 @@ export type OrderInbound = {
   process_start_time: string | null;
   process_status: number;
 };
+
+export type MaterialInbound = {
+  id: number;
+  materialItemId: number;        // materialItem 객체 대신 ID만 전달할 경우
+  supplierName: string;
+  itemName: string;
+  itemCode: string;
+  specQty: number;
+  specUnit: string;
+  manufacturer: string;
+  manufacteDate: string;         // 'YYYY-MM-DD' 형식
+  qty: number;
+  inboundDate: string;           // 'YYYY-MM-DD' 형식
+  inboundNo: string;
+  totalQty: number;
+}
